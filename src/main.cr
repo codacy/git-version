9f937e5f6d5f5013df53dcd8dd8cbdf60ddc1beb
Paulo Martins <paulo.pontes.m@gmail.com>
2020-12-16 18:10:32 +0000

    rename latest-version to previous-version

--- src/main.cr
+++ src/main.cr
@@ -4,7 +4,7 @@ require "file_utils"
 
 require "./git-version"
 
-latest_version = false
+previous_version = false
 dev_branch = "dev"
 release_branch = "master"
 prefix = ""
@@ -19,7 +19,7 @@ OptionParser.parse! do |parser|
   parser.on("-r BRANCH", "--release-branch=BRANCH", "Specifies the release branch") { |branch| release_branch = branch }
   parser.on("-p PREFIX", "--version-prefix=PREFIX", "Specifies a version prefix") { |p| prefix = p }
   parser.on("-l PATH", "--log-paths=PATH", "") { |path| log_paths = path }
-  parser.on("--latest-version", "Returns the latest tag instead of calculating a new one") { latest_version=true }
+  parser.on("--previous-version", "Returns the previous tag instead of calculating a new one") { previous_version=true }
   parser.on("-h", "--help", "Show this help") { puts parser }
   parser.invalid_option do |flag|
     STDERR.puts "ERROR: #{flag} is not a valid option."
@@ -30,8 +30,8 @@ end
 
 git = GitVersion::Git.new(dev_branch, release_branch, folder, prefix, log_paths)
 
-if latest_version
-  puts "#{git.get_latest_version}"
+if previous_version
+  puts "#{git.get_previous_version}"
 else
   puts "#{git.get_new_version}"
 end
